3c7fa88e8d4b23cf3c5b7bc8191dd6d7d52c5512
lihong.ji <294672570@qq.com>
2024-03-26 18:58:34 +0100

    VAE and CVAE are finished

--- VariationalAutoEncoder/model.py
+++ VariationalAutoEncoder/model.py
@@ -1,14 +1,13 @@
 import torch
 import torch.nn as nn
 
-import torch.nn.functional as F
-
 
 class VariationalAutoencoder(nn.Module):
-    def __init__(self, dim_data, dim_latent, conditional=False):
+    def __init__(self, dim_data, dim_latent, conditional=False, label_num=None):
         super(VariationalAutoencoder, self).__init__()
-        self.encoder = Encoder(dim_data, dim_latent, conditional)
-        self.decoder = Decoder(dim_data, dim_latent, conditional)
+        self.encoder = Encoder(dim_data, dim_latent, conditional, label_num)
+        self.decoder = Decoder(dim_data, dim_latent, conditional, label_num)
+        self.conditional = conditional
 
     @staticmethod
     def reparameterize(mu, sigma):
@@ -17,18 +16,25 @@ class VariationalAutoencoder(nn.Module):
 
         return z_reparameterized
 
-    def forward(self, data):
-        mu, sigma = self.encoder(data)
-        z = self.reparameterize(mu, sigma)
+    def forward(self, data, label=None):
+        if self.conditional:
+            onehot_label = nn.functional.one_hot(label, num_classes=10)
+            mu, sigma = self.encoder(data, onehot_label)
+            z = self.reparameterize(mu, sigma)
+            z = torch.cat([z, onehot_label], dim=1)
+        else:
+            mu, sigma = self.encoder(data)
+            z = self.reparameterize(mu, sigma)
+
         reconstructed_data = self.decoder(z)
 
         return reconstructed_data, mu, sigma
 
 
 class Encoder(nn.Module):
-    def __init__(self, dim_data, dim_latent, conditional):
+    def __init__(self, dim_data, dim_latent, conditional, label_num=None):
         super(Encoder, self).__init__()
-        self.dim_data = dim_data
+        self.dim_data = dim_data + label_num if conditional else dim_data
         self.dim_latent = dim_latent
         self.dim_hid1 = 256
         self.dim_hid2 = 64
@@ -43,7 +49,10 @@ class Encoder(nn.Module):
         self.nn_mu = nn.Linear(self.dim_hid2, self.dim_latent)
         self.nn_sigma = nn.Linear(self.dim_hid2, self.dim_latent)
 
-    def forward(self, data):
+    def forward(self, data, onehot_label=None):
+        if self.conditional:
+            data = torch.cat([data, onehot_label], dim=-1)
+
         for layer in self.MLP:
             data = layer(data)
         mu = self.nn_mu(data)
@@ -53,10 +62,10 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, dim_data, dim_latent, conditional):
+    def __init__(self, dim_data, dim_latent, conditional, label_num=None):
         super(Decoder, self).__init__()
         self.dim_data = dim_data
-        self.dim_latent = dim_latent
+        self.dim_latent = dim_latent + label_num if conditional else dim_latent
         self.dim_hid1 = 64
         self.dim_hid2 = 256
 
--- VariationalAutoEncoder/train.py
+++ VariationalAutoEncoder/train.py
@@ -12,29 +12,38 @@ epoch = 20
 dim_data = 28 * 28
 dim_latent = 20
 batch_size = 128
+if_conditional = False
+
+label_num = 10
+
+generated_num = 2
 
 device = torch.device("cuda" if torch.cuda.is_available() else "")
 
 dataset = MNIST(root='data', train=True, transform=transforms.ToTensor(), download=True)
 data_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
 
-model = VariationalAutoencoder(dim_data, dim_latent, conditional=False).to(device)
+model = VariationalAutoencoder(dim_data, dim_latent, conditional=if_conditional, label_num=label_num).to(device)
 optimizer = optim.AdamW(model.parameters(), lr=5e-4)
 
 
-def loss_fn(recon_x, x, _mu, _sigma):
+def loss_fn(recon_x, _x, _mu, _sigma):
     BCE = nn.BCELoss(reduction='sum')
-    BCE_loss = BCE(recon_x, x)
+    BCE_loss = BCE(recon_x, _x)
     KL = -torch.sum(1 + torch.log(_sigma.pow(2)) - _mu.pow(2) - _sigma.pow(2))
 
-    return (BCE_loss + KL) / x.size(0)
+    return (BCE_loss + KL) / _x.size(0)
 
 
 for epoch in range(epoch):
     loop = tqdm(enumerate(data_loader))
-    for i, (data, _) in loop:
+    for i, (data, label) in loop:
+        # print(data.shape, label.shape)
+        #  [128, 1, 28, 28] -> [128, 1, 768],    [128] -> [128, 1, 10]  ===> concat [128, 1, 778]
         data = data.view(data.shape[0], -1).to(device)
-        reconstructed_data, mu, sigma = model(data)
+        label = label.view(label.shape[0]).to(device)
+
+        reconstructed_data, mu, sigma = model(data, label)
 
         loss = loss_fn(reconstructed_data, data, mu, sigma)
 
@@ -46,9 +55,15 @@ for epoch in range(epoch):
 
 
 model.eval()
-for i in range(5):
-    epsilon = torch.randn(dim_latent).to(device)
-    x = model.decoder(epsilon)
+for i in range(10):
+    epsilon = torch.randn(dim_latent).to(device).unsqueeze(0)
+    if if_conditional:
+        onehot_label = torch.tensor(generated_num).view(-1).to(device)
+        label = nn.functional.one_hot(onehot_label, num_classes=10).view(1, -1)
+
+        latent_variable = torch.cat([epsilon, label], dim=1).view(-1)
+    else:
+        latent_variable = epsilon.view(-1)
+    x = model.decoder(latent_variable)
     x = x.view(-1, 1, 28, 28)
-    print(x.shape)
-    save_image(x, f'res/{i}_generated_digit.png')
+    save_image(x, f'res/VAE/{i}_generated.png')
